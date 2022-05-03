1c588ad384196d0a48f618bf31d99381f8af7a0f
Ghost-lzw <lanzongwei541@gmail.com>
2022-05-03 13:53:14 +0800

    support MC Dag select

---
+++ Cpu0SEISelDAGToDAG.h
@@ -0,0 +1,53 @@
+// Copyright 2022 All Rights Reserved.
+// Author: lanzongwei541@gmail.com (lanzongwei)
+//
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//===----------------------------------------------------------------------===//
+//
+// Subclass of Cpu0DAGToDAGISel specialized for cpu032.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_CPU0_CPU0SEISELDAGTODAG_H
+#define LLVM_LIB_TARGET_CPU0_CPU0SEISELDAGTODAG_H
+
+#include "Cpu0ISelDAGToDAG.h"
+
+namespace llvm {
+
+class Cpu0SEDAGToDAGISel : public Cpu0DAGToDAGISel {
+
+public:
+  explicit Cpu0SEDAGToDAGISel(Cpu0TargetMachine &TM, CodeGenOpt::Level OL)
+      : Cpu0DAGToDAGISel(TM, OL) {}
+
+private:
+  bool runOnMachineFunction(MachineFunction &MF) override;
+
+  bool trySelect(SDNode *Node) override;
+
+  void processFunctionAfterISel(MachineFunction &MF) override;
+
+  // Insert instructions to initialize the global base register in the
+  // first MBB of the function.
+  //  void initGlobalBaseReg(MachineFunction &MF);
+
+#if 0 // CH >= CH4_1
+  std::pair<SDNode *, SDNode *> selectMULT(SDNode *N, unsigned Opc,
+                                           const SDLoc &DL, EVT Ty, bool HasLo,
+                                           bool HasHi);
+#endif
+
+#if 0 // CH >= CH7_1
+  void selectAddESubE(unsigned MOp, SDValue InFlag, SDValue CmpLHS,
+                      const SDLoc &DL, SDNode *Node) const;
+#endif
+};
+
+FunctionPass *createCpu0SEISelDag(Cpu0TargetMachine &TM,
+                                  CodeGenOpt::Level OptLevel);
+
+} // namespace llvm
+
+#endif
